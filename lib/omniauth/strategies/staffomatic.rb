180f37cc72e1c84a9f6623f8f9053537e17f7ba1
kalle saas <kalle@easypep.de>
2014-08-08 22:19:32 +0200

    removed scopes add user specific UID

--- lib/omniauth/strategies/staffomatic.rb
+++ lib/omniauth/strategies/staffomatic.rb
@@ -1,50 +1,53 @@
-require 'omniauth/strategies/oauth2'
+require 'omniauth-oauth2'
 
 module OmniAuth
   module Strategies
     class Staffomatic < OmniAuth::Strategies::OAuth2
-      # Available scopes: content themes products customers orders script_tags shipping
-      # read_*  or write_*
-      DEFAULT_SCOPE = 'read_users'
-
-      option :client_options, {
-        :authorize_url => '/oauth/authorize',
-        :token_url => '/oauth/access_token'
-      }
-
       option :name, :staffomatic
 
-      option :callback_url
+      option :client_options, {
+        authorize_url: '/oauth/authorize'
+      }
 
+      option :callback_url
       option :provider_ignores_state, true
       option :staffomatic_domain, 'staffomatic.com'
+      option :easypep_domain,     'easypep.de'
+      option :development_domain, 'staffomatic-api.dev'
 
-      uid { URI.parse(options[:client_options][:site]).host }
+      uid do
+        raw_info["id"]
+      end
+
+      info do
+        {
+          email: raw_info["email"],
+          name: raw_info["full_name"]
+        }
+      end
+
+      def raw_info
+        @raw_info ||= access_token.get('/api/v3/user').parsed["users"].try(:first)
+      end
 
       def valid_site?
-        !!(/\A(https|http)\:\/\/[a-zA-Z0-9][a-zA-Z0-9\-]*\.#{Regexp.quote(options[:staffomatic_domain])}[\/]?\z/ =~ options[:client_options][:site])
+        !!(/\A(https|http)\:\/\/[a-zA-Z0-9][a-zA-Z0-9\-]*\.(#{Regexp.quote(options[:development_domain])}|#{Regexp.quote(options[:staffomatic_domain])}|#{Regexp.quote(options[:easypep_domain])})[\/]?\z/ =~ options[:client_options][:site])
       end
 
-      def fix_https
-        options[:client_options][:site].gsub!(/\Ahttp\:/, 'https:')
-      end
-
-      def setup_phase
-        super
-        fix_https
-      end
+      # well, we should do that, but it's hard in dev mode!
+      # def fix_https
+      #   options[:client_options][:site].gsub!(/\Ahttp\:/, 'https:')
+      # end
+      # def setup_phase
+      #   super
+      #   fix_https
+      # end
 
       def request_phase
         if valid_site?
           super
         else
           fail!(:invalid_site)
-        end
-      end
-
-      def authorize_params
-        super.tap do |params|
-          params[:scope] ||= DEFAULT_SCOPE
         end
       end
 
